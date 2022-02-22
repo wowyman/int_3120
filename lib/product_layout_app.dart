daccbd78a6a35e40ef422aa5ea263abb86034947
Mai Cong Danh 19021233 <62579415+wowyman@users.noreply.github.com>
2022-02-22 20:57:38 +0700

    StatefulWidget

--- lib/product_layout_app.dart
+++ lib/product_layout_app.dart
@@ -37,6 +37,72 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+class RatingBox extends StatefulWidget {
+  @override
+  _RatingBoxState createState() =>
+      _RatingBoxState();
+}
+
+class _RatingBoxState extends State<RatingBox> {
+  int _rating = 0;
+  void _setRatingAsOne() {
+    setState( () {
+      _rating = 1;
+    });
+  }
+  void _setRatingAsTwo() {
+    setState( () {
+      _rating = 2;
+    });
+  }
+  void _setRatingAsThree() {
+    setState( () {
+      _rating = 3;
+    });
+  }
+  Widget build(BuildContext context) {
+    double _size = 20;
+    print(_rating);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 1 ? Icon(Icons.star, size: _size,) :
+            Icon(Icons.star_border, size: _size,)),
+            color: Colors.red[500],
+            onPressed: _setRatingAsOne,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 2 ? Icon(Icons.star, size: _size,) :
+            Icon(Icons.star_border, size: _size,)),
+            color: Colors.red[500],
+            onPressed: _setRatingAsTwo,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 3 ? Icon(Icons.star, size: _size,) :
+            Icon(Icons.star_border, size: _size,)),
+            color: Colors.red[500],
+            onPressed: _setRatingAsThree,
+            iconSize: _size,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class ProductBox extends StatelessWidget {
   ProductBox({Key? key, required this.name, required this.description, required this.price, required this.image})
       : super(key: key);
@@ -46,10 +112,12 @@ class ProductBox extends StatelessWidget {
   final String image;
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.all(2), height: 120, child: Card(
+        padding: EdgeInsets.all(2), height: 140, child: Card(
         child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
-          Image.asset("assets/" +image), Expanded(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Image.asset("assets/" +image),
+              Expanded(
               child: Container(
                   padding: EdgeInsets.all(5), child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -57,6 +125,7 @@ class ProductBox extends StatelessWidget {
                   Text(this.name, style: TextStyle(fontWeight:
                   FontWeight.bold)), Text(this.description),
                   Text("Price: " + this.price.toString()),
+                  RatingBox(),
                 ],
               )
               )
